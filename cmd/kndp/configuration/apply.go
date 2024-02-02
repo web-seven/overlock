d9e701bd1022bed20dd29d4e21bc2ef193275493
Catalin <catalincretu1234@gmail.com>
2024-02-02 12:20:49 +0200

    adjusted kndp cli tool

--- cmd/kndp/configuration/apply.go
+++ cmd/kndp/configuration/apply.go
@@ -3,13 +3,11 @@ package configuration
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/url"
-	"os"
-	"os/exec"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
@@ -21,17 +19,25 @@ import (
 )
 
 type applyCmd struct {
-	Link string `arg:"" required:"" help:"Link URL to Crossplane configuration to be applied to Environment in next format: xpkg.upbound.io/upbound/platform-ref-aws:v0.6.0."`
+	Link string `arg:"" required:"" help:"Link URL to Crossplane configuration to be applied to Environment."`
+}
+type ResourceParams struct {
+	dynamic   dynamic.Interface
+	ctx       context.Context
+	group     string
+	version   string
+	resource  string
+	namespace string
 }
 
-func GetResources(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string, namespace string) ([]unstructured.Unstructured, error) {
+func GetResources(p ResourceParams) ([]unstructured.Unstructured, error) {
 	resourceId := schema.GroupVersionResource{
-		Group:    group,
-		Version:  version,
-		Resource: resource,
+		Group:    p.group,
+		Version:  p.version,
+		Resource: p.resource,
 	}
-	list, err := dynamic.Resource(resourceId).Namespace(namespace).
-		List(ctx, metav1.ListOptions{})
+	list, err := p.dynamic.Resource(resourceId).Namespace(p.namespace).
+		List(p.ctx, metav1.ListOptions{})
 
 	if err != nil {
 		return nil, err
@@ -40,15 +46,48 @@ func GetResources(dynamic dynamic.Interface, ctx context.Context, group string, 
 	return list.Items, nil
 }
 
+type ObjectRef struct {
+	Name string `json:"name"`
+	Kind string `json:"kind"`
+}
+type ConfigurationRevisions struct {
+	Spec struct {
+		Image string `json:"image"`
+	} `json:"spec"`
+	Status struct {
+		ObjectRefs []ObjectRef `json:"objectRefs"`
+	}
+}
+
 func (c *applyCmd) Run(p pterm.TextPrinter) error {
-
+	//call k8s api with spec.image link to match the right configuration revision, after that extract status.objectRefs of it with kind xrd and get their names.
 	var xrds []string
-
 	ctx := context.Background()
 	config := ctrl.GetConfigOrDie()
 	dynamicClient := dynamic.NewForConfigOrDie(config)
-	items, _ := GetResources(dynamicClient, ctx, "apiextensions.crossplane.io", "v1", "compositeresourcedefinitions", "")
+	var ResourceParams = ResourceParams{
+		dynamic:   dynamicClient,
+		ctx:       ctx,
+		group:     "pkg.crossplane.io",
+		version:   "v1",
+		resource:  "configurationrevisions",
+		namespace: "",
+	}
 
+	items, _ := GetResources(ResourceParams)
+	for _, item := range items {
+		image := &ConfigurationRevisions{}
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, image); err != nil {
+			return fmt.Errorf("error converting Unstructured to Meal: %v", err)
+		}
+		if image.Spec.Image == c.Link {
+			for _, objectRef := range image.Status.ObjectRefs {
+				if objectRef.Kind == "CompositeResourceDefinition" {
+					xrds = append(xrds, objectRef.Name)
+				}
+			}
+		}
+	}
 	chartName := "crossplane"
 	repoURL, err := url.Parse("https://charts.crossplane.io/stable")
 	if err != nil {
@@ -71,43 +110,47 @@ func (c *applyCmd) Run(p pterm.TextPrinter) error {
 		fmt.Printf("Error: %v\n", err)
 	}
 
-	pterm.Success.Println("Crossplane upgrade completed successfully.")
+	pterm.Success.Println("KNDP configuration applied successfully.")
 
-	//Display list of Crossplane configurations
+	var createResource bool
+	formConfirm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Would you like to create a resource from kndp configuration?").
+				Affirmative("Yes!").
+				Negative("No.").
+				Value(&createResource),
+		),
+	)
+	// Display a list of Crossplane configurations
 	form := huh.NewForm(
 		huh.NewGroup(
 			func() *huh.MultiSelect[string] {
 				multiSelect := huh.NewMultiSelect[string]().
-					Title("Select Crossplane configuration to create resource:")
+					Title("Select KNDP configuration to create a resource:")
 				var options []huh.Option[string]
-				for _, item := range items {
-					options = append(options, huh.NewOption(item.GetName(), item.GetName()))
+				for _, xrd := range xrds {
+					options = append(options, huh.NewOption(xrd, xrd).Selected(false))
 				}
 				multiSelect.Options(options...)
 				multiSelect.Value(&xrds)
+
 				return multiSelect
 			}(),
 		),
 	)
-	err = form.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	selectedValues := xrds
-	if len(selectedValues) > 0 {
-		for _, value := range selectedValues {
-			//This will execute kndp resource create <value> --interactive for every value selected
-			cmd := exec.Command("kndp", "resource", "create", value, "--interactive")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-
-			if err := cmd.Run(); err != nil {
-				log.Fatal(err)
-			}
+	formConfirm.Run()
+	if createResource {
+		if len(xrds) > 0 {
+			form.Run()
+		} else {
+			fmt.Println("No resource to create from: ", c.Link)
 		}
-	} else {
-		fmt.Println("No values selected.")
+		selectedValues := xrds
+		for _, value := range selectedValues {
+			fmt.Println("Creating KNDP resource from: ", value, "\n")
+		}
 	}
 
 	return nil
