d692e5b880215eb105069d5ad593224b94188242
Catalin <catalincretu1234@gmail.com>
2024-02-22 16:20:45 +0200

    reused configuration.GetManager for installer

--- internal/provider/install.go
+++ internal/provider/install.go
@@ -1,30 +1,11 @@
 package provider
 
 import (
-	"net/url"
-
 	"github.com/charmbracelet/log"
 
+	"github.com/kndpio/kndp/internal/configuration"
 	"k8s.io/client-go/rest"
-
-	"github.com/kndpio/kndp/internal/install"
-	"github.com/kndpio/kndp/internal/install/helm"
 )
-
-func GetManager(config *rest.Config, logger *log.Logger) install.Manager {
-	chartName := "crossplane"
-
-	repoURL, err := url.Parse("https://charts.crossplane.io/stable")
-	if err != nil {
-		logger.Errorf(" %v\n", err)
-	}
-	installer, err := helm.NewManager(config, chartName, repoURL, helm.WithReuseValues(true))
-	installer.GetCurrentVersion()
-	if err != nil {
-		logger.Errorf(" %v\n", err)
-	}
-	return installer
-}
 
 func InstallProvider(provider string, config *rest.Config, logger *log.Logger) {
 
@@ -34,7 +15,7 @@ func InstallProvider(provider string, config *rest.Config, logger *log.Logger) {
 		},
 	}
 
-	installer := GetManager(config, logger)
+	installer := configuration.GetManager(config, logger)
 
 	err := installer.Upgrade("", parameters)
 	if err != nil {
