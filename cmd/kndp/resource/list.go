3ed9b05c28fb078ff49d84385bdb234bf5ce44ad
Catalin <catalincretu1234@gmail.com>
2024-02-27 10:43:39 +0200

    Enhanced 'kndp resource list' command with label-based resource filtering.

--- cmd/kndp/resource/list.go
+++ cmd/kndp/resource/list.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/charmbracelet/log"
+	"github.com/ghodss/yaml"
 	"github.com/kndpio/kndp/internal/resources"
 	"github.com/rodaine/table"
 
@@ -15,16 +16,29 @@ type listCmd struct {
 }
 
 func (listCmd) Run(ctx context.Context, config *rest.Config, dynamicClient *dynamic.DynamicClient, logger *log.Logger) error {
-	tbl := table.New("NAME", "API-VERSION", "KIND", "LABELS")
+	tbl := table.New("NAME", "API-VERSION", "KIND", "CREATION-DATE", "UPDATE-DATE")
+	logger.SetReportTimestamp(false)
 
 	xresources := resources.GetXResources(ctx, dynamicClient, logger)
 	for _, resource := range xresources {
-		labels := resources.ExtractLabels(resource.GetLabels())
-		tbl.AddRow(resource.GetName(), resource.GetAPIVersion(), resource.GetKind(), labels)
+		managedBy := resource.GetLabels()["app.kubernetes.io/managed-by"]
+		if managedBy == "kndp" {
+			createdLabel := resource.GetLabels()["creation-date"]
+			updatedLabel := resource.GetLabels()["update-date"]
+			tbl.AddRow(resource.GetName(), resource.GetAPIVersion(), resource.GetKind(), createdLabel, updatedLabel)
 
+			jsonFormat, _ := resource.MarshalJSON()
+			yamlFormat, _ := yaml.JSONToYAML(jsonFormat)
+			logger.Printf("\n%s JSON: \n%s\n", resource.GetName(), string(jsonFormat))
+			logger.Printf("%s YAML: \n%s\n", resource.GetName(), string(yamlFormat))
+		}
 	}
 
-	tbl.Print()
+	if len(xresources) > 0 {
+		tbl.Print()
+	} else {
+		logger.Info("No resources found managed by kndp.")
+	}
 
 	return nil
 }
