e78061c84ed4f25bc8d02ab3410ce19f132543ee
Catalin <catalincretu1234@gmail.com>
2024-02-28 15:05:48 +0200

    Disabled resource creation prompting after configuration apply.

--- cmd/kndp/configuration/apply.go
+++ cmd/kndp/configuration/apply.go
@@ -3,16 +3,11 @@ package configuration
 import (
 	"context"
 
-	crossv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
-	"github.com/kndpio/kndp/cmd/kndp/resource"
 	"github.com/kndpio/kndp/internal/configuration"
-	"github.com/kndpio/kndp/internal/xrd"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 
-	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/log"
 )
 
@@ -20,59 +15,7 @@ type applyCmd struct {
 	Link string `arg:"" required:"" help:"Link URL to Crossplane configuration to be applied to Environment."`
 }
 
-func handleForm(ctx context.Context, client *dynamic.DynamicClient, xrds []string, Link string, logger *log.Logger) {
-	var createResource bool
-	formConfirm := huh.NewForm(
-		huh.NewGroup(
-			huh.NewConfirm().
-				Title("Would you like to create a resource from kndp configuration?").
-				Affirmative("Yes!").
-				Negative("No.").
-				Value(&createResource),
-		),
-	)
-	// Display a list of Crossplane configurations
-	form := huh.NewForm(
-		huh.NewGroup(
-			func() *huh.MultiSelect[string] {
-				multiSelect := huh.NewMultiSelect[string]().
-					Title("Select KNDP configuration to create a resource:")
-				var options []huh.Option[string]
-				for _, xrd := range xrds {
-					options = append(options, huh.NewOption(xrd, xrd).Selected(false))
-				}
-				multiSelect.Options(options...)
-				multiSelect.Value(&xrds)
-
-				return multiSelect
-			}(),
-		),
-	)
-	if len(xrds) > 0 {
-		formConfirm.Run()
-		if createResource {
-			form.Run()
-			selectedValues := xrds
-			for _, value := range selectedValues {
-				xrd := crossv1.CompositeResourceDefinition{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: value,
-					},
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "apiextensions.k8s.io/v1",
-						Kind:       "customresourcedefinitions",
-					},
-				}
-				resource.CreateXResource(ctx, xrd, client, logger)
-			}
-		}
-	}
-}
-
 func (c *applyCmd) Run(ctx context.Context, config *rest.Config, dynamicClient *dynamic.DynamicClient, logger *log.Logger) error {
 	configuration.ApplyConfiguration(c.Link, config, logger)
-	xrds := xrd.GetXRDs(c.Link, ctx, config, dynamicClient)
-	handleForm(ctx, dynamicClient, xrds, c.Link, logger)
-
 	return nil
 }
