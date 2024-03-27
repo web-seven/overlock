4e567e0504c5d65ee9881b32fb1ecff8c4f31392
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2024-03-27 19:29:16 +0200

    made merge on cfg apply

--- internal/configuration/apply.go
+++ internal/configuration/apply.go
@@ -10,18 +10,33 @@ import (
 
 func ApplyConfiguration(Link string, config *rest.Config, logger *log.Logger) {
 
-	parameters := map[string]interface{}{
-		"configuration": map[string]interface{}{
-			"packages": []string{Link},
-		},
-	}
-
 	installer, err := engine.GetEngine(config)
 	if err != nil {
 		logger.Errorf(" %v\n", err)
 	}
 
-	err = installer.Upgrade("", parameters)
+	release, _ := installer.GetRelease()
+
+	if release.Config == nil {
+		release.Config = map[string]interface{}{
+			"configuration": map[string]interface{}{
+				"packages": []string{Link},
+			},
+		}
+	} else if release.Config["configuration"] == nil {
+		release.Config["configuration"] = map[string]interface{}{
+			"packages": []string{Link},
+		}
+	} else {
+		configs := release.Config["configuration"].(map[string]interface{})
+		configs["packages"] = append(
+			configs["packages"].([]interface{}),
+			Link,
+		)
+		release.Config["configuration"] = configs
+	}
+
+	err = installer.Upgrade("", release.Config)
 	if err != nil {
 		logger.Errorf(" %v\n", err)
 	}
