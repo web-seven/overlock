9abb0549e5ea0ac0a8215888d85ea086434d170b
Catalin <catalincretu1234@gmail.com>
2024-04-16 16:22:55 +0300

    set datetime log to false for version flag

--- cmd/kndp/main.go
+++ cmd/kndp/main.go
@@ -31,6 +31,7 @@ var Version = "development"
 func (v VersionFlag) Decode(ctx *kong.DecodeContext) error { return nil }
 func (v VersionFlag) IsBool() bool                         { return true }
 func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
+	log.SetReportTimestamp(false)
 	log.Print(vars["version"])
 	app.Exit(0)
 	return nil
