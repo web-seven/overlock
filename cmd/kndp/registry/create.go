98eb8a221605aadfe6b69ef2c5ac317ab3081ae4
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2024-06-10 19:30:42 +0300

    made optional credential parameters for local registry creation

--- cmd/kndp/registry/create.go
+++ cmd/kndp/registry/create.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/charmbracelet/log"
-	"github.com/go-playground/validator/v10"
 
 	"github.com/kndpio/kndp/internal/registry"
 
@@ -17,38 +16,28 @@ const (
 )
 
 type createCmd struct {
-	RegistryServer string `required:"" help:"is your Private Registry FQDN."`
-	Username       string `required:"" help:"is your Username."`
-	Password       string `required:"" help:"is your Password."`
-	Email          string `required:"" help:"is your Email."`
+	RegistryServer string `help:"is your Private Registry FQDN."`
+	Username       string `help:"is your Username."`
+	Password       string `help:"is your Password."`
+	Email          string `help:"is your Email."`
 	Default        bool   `help:"Set registry as default."`
 	Local          bool   `help:"Create local registry."`
 }
 
 func (c *createCmd) Run(ctx context.Context, client *kubernetes.Clientset, config *rest.Config, logger *log.Logger) error {
-
 	reg := registry.New(c.RegistryServer, c.Username, c.Password, c.Email)
 	reg.SetDefault(c.Default)
 	reg.SetLocal(c.Local)
-	verr := reg.Validate(logger)
-	if verr != nil {
-		errs := verr.(validator.ValidationErrors)
-		for _, err := range errs {
-			logger.Errorf(fieldErrMsg, err.Field(), err.Tag())
-		}
-		return nil
+	err := reg.Validate(ctx, client, logger)
+	if err != nil {
+		return err
 	}
 
-	if reg.Exists(ctx, client) {
-		logger.Info("Secret for this registry server already exists.")
-		return nil
-	}
-	err := reg.Create(ctx, config, logger)
+	err = reg.Create(ctx, config, logger)
 	if err != nil {
 		logger.Error(err)
 	} else {
 		logger.Info("Registry created successfully.")
 	}
-
 	return nil
 }
