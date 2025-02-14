52656bc89d2db143ce4713ed1b1bb55425138137
Finlay Sawyer <18363677+finlaysawyer@users.noreply.github.com>
2025-02-14 21:16:22 +0000

    env: allow engine config to be specified on cluster creation

--- cmd/overlock/environment/create.go
+++ cmd/overlock/environment/create.go
@@ -8,12 +8,13 @@ import (
 )
 
 type createCmd struct {
-	Name      string `arg:"" requried:"" help:"Name of environment."`
-	HttpPort  int    `optional:"" short:"p" help:"Http host port for mapping" default:"80"`
-	HttpsPort int    `optional:"" short:"s" help:"Https host port for mapping" default:"443"`
-	Context   string `optional:"" short:"c" help:"Kubernetes context where Environment will be created."`
-	Engine    string `optional:"" short:"e" help:"Specifies the Kubernetes engine to use for the runtime environment." default:"kind"`
-	MountPath string `optional:"" help:"Path for mount to /storage host directory. By default no mounts."`
+	Name         string `arg:"" requried:"" help:"Name of environment."`
+	HttpPort     int    `optional:"" short:"p" help:"Http host port for mapping" default:"80"`
+	HttpsPort    int    `optional:"" short:"s" help:"Https host port for mapping" default:"443"`
+	Context      string `optional:"" short:"c" help:"Kubernetes context where Environment will be created."`
+	Engine       string `optional:"" short:"e" help:"Specifies the Kubernetes engine to use for the runtime environment." default:"kind"`
+	EngineConfig string `optional:"" help:"Path to the configuration file for the engine. Currently supported for kind clusters."`
+	MountPath    string `optional:"" help:"Path for mount to /storage host directory. By default no mounts."`
 }
 
 func (c *createCmd) Run(ctx context.Context, logger *zap.SugaredLogger) error {
@@ -23,5 +24,6 @@ func (c *createCmd) Run(ctx context.Context, logger *zap.SugaredLogger) error {
 		WithHttpsPort(c.HttpsPort).
 		WithContext(c.Context).
 		WithMountPath(c.MountPath).
+		WithEngineConfig(c.EngineConfig).
 		Create(ctx, logger)
 }
