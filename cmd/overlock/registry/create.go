a9e307594b9bb97509115e6d96fa10f057fd7d7e
Web7Agent <web7devs@gmail.com>
2026-04-03 16:07:35 +0300

    feat: add --label flag to registry create command (#445)

--- cmd/overlock/registry/create.go
+++ cmd/overlock/registry/create.go
@@ -2,6 +2,8 @@ package registry
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -16,13 +18,14 @@ const (
 )
 
 type createCmd struct {
-	RegistryServer string `help:"is your Private Registry FQDN."`
-	Username       string `help:"is your Username."`
-	Password       string `help:"is your Password."`
-	Email          string `help:"is your Email."`
-	Default        bool   `help:"Set registry as default."`
-	Local          bool   `help:"Create local registry."`
-	Context        string `short:"c" help:"Kubernetes context where registry will be created."`
+	RegistryServer string   `help:"is your Private Registry FQDN."`
+	Username       string   `help:"is your Username."`
+	Password       string   `help:"is your Password."`
+	Email          string   `help:"is your Email."`
+	Default        bool     `help:"Set registry as default."`
+	Local          bool     `help:"Create local registry."`
+	Context        string   `short:"c" help:"Kubernetes context where registry will be created."`
+	Label          []string `short:"l" help:"Label to attach to the registry secret in key:value format. Can be specified multiple times."`
 }
 
 func (c *createCmd) Run(ctx context.Context, client *kubernetes.Clientset, config *rest.Config, logger *zap.SugaredLogger) error {
@@ -33,6 +36,18 @@ func (c *createCmd) Run(ctx context.Context, client *kubernetes.Clientset, confi
 	reg.SetDefault(c.Default)
 	reg.SetLocal(c.Local)
 	reg.WithContext(c.Context)
+
+	if len(c.Label) > 0 {
+		labels := make(map[string]string, len(c.Label))
+		for _, l := range c.Label {
+			parts := strings.SplitN(l, ":", 2)
+			if len(parts) != 2 {
+				return fmt.Errorf("invalid label format %q: expected key:value", l)
+			}
+			labels[parts[0]] = parts[1]
+		}
+		reg.WithLabels(labels)
+	}
 
 	if reg.Exists(ctx, client) {
 		logger.Infof("Registry '%s' already exists. Using existing registry.", reg.Name)
