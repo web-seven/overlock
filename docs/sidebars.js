4b974c4fc1ecb166e32139c0dcd633544dcf0ca8
Web7Agent <web7devs@gmail.com>
2026-04-23 12:39:25 +0300

    #449: feat(docs): set up Docusaurus in ./docs directory (#450)

---
+++ docs/sidebars.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    'guide/getting-started',
+    'guide/environments',
+    'guide/configurations',
+    'guide/providers',
+    'guide/functions',
+    'guide/registries',
+    'guide/resources',
+    'guide/local-nodes',
+    'guide/remote-nodes',
+    'guide/plugins',
+    'examples',
+    'troubleshoot',
+    'development',
+  ],
+};
+
+module.exports = sidebars;
